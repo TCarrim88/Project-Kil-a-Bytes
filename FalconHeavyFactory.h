3584d895c7f947ab08d8c14fdd7ae7e3da26398f
juwiCarrim <93875648+juwiCarrim@users.noreply.github.com>
2021-11-17 15:57:44 +0200

    Delete FalconHeavyFactory.h

--- FalconHeavyFactory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FalconHeavyFactory_h__
-#define __FalconHeavyFactory_h__
-#include "FalconFactory.h"
-
-class FalconHeavyFactory: public FalconFactory{
-    public:
-    Falcon* createFalcon();
-};
-#endif;
