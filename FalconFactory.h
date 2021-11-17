0249765e0b851ddf49e0600ead4a0954fa134527
Yaasir25 <93663409+Yaasir25@users.noreply.github.com>
2021-11-17 16:00:26 +0200

    Delete FalconFactory.h

--- FalconFactory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FalconFactory_h__
-#define __FalconFactory_h__
-#include "Falcon.h"
-//AbstrFact: AbstrFact
-class FalconFactory{
-    public:
-    virtual Falcon* createFalcon();
-};
-#endif;
