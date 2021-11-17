27eccdf798f8aebd8dd006727c4881f9df033ae8
Yaasir25 <93663409+Yaasir25@users.noreply.github.com>
2021-11-17 16:00:33 +0200

    Delete Falcon9Factory.h

--- Falcon9Factory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __Falcon9Factory_h__
-#define __Falcon9Factory_h__
-#include "FalconFactory.h"
-
-class Falcon9Factory: public FalconFactory{
-    public:
-    Falcon* createFalcon();
-};
-#endif;
