4d6dc612721d3239e57305fd4e6a83f14ed39fb8
Yaasir25 <93663409+Yaasir25@users.noreply.github.com>
2021-11-17 16:00:48 +0200

    Delete Falcon9CoreFact.h

--- Falcon9CoreFact.h
+++
@@ -1,9 +0,0 @@
-#ifndef __Falcon9CoreFact_h__
-#define __Falcon9CoreFact_h__
-#include "AbstractCCFactory.h"
-
-class Falcon9CoreFactory{
-    public:
-    CraftComponent* createEngine();
-};
-#endif;
